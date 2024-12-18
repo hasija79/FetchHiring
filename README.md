fd19f7b169978c2fc1cfc5c3761ea2cfa4af59ae
hasija79 <hasija79@users.noreply.github.com>
2024-12-18 15:43:20 -0600

    Create README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+<image src="https://github.com/user-attachments/assets/0af81098-c74c-48b0-befd-4f974e7ccaaa" width=300 height=640>
+<image src="https://github.com/user-attachments/assets/0d43fb58-0b9c-43a9-8ea4-1c9ec36e0c94" width=300 height=640>
+<image src="https://github.com/user-attachments/assets/54a35a25-a1f3-4c53-bf92-16dba936e0c7" width=300 height=640>
--- app/src/main/java/com/sample/fetch/hiring/ui/MainActivity.kt
+++ app/src/main/java/com/sample/fetch/hiring/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sample.fetch.hiring
+package com.sample.fetch.hiring.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -14,7 +14,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import com.sample.fetch.hiring.ui.HiringScreen
+import com.sample.fetch.hiring.R
 import com.sample.fetch.hiring.app.theme.FetchHiringTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -29,26 +29,27 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun AppScaffold() {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = colorScheme.primary,
-                    titleContentColor = colorScheme.background,
-                ),
-                title = {
-                    Text(text = stringResource(R.string.app_name))
-                }
-            )
-        },
-        content = { innerPadding ->
-            HiringScreen(modifier = Modifier.padding(innerPadding))
-        }
-    )
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun AppScaffold() {
+        Scaffold(
+            modifier = Modifier.Companion.fillMaxSize(),
+            topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = MaterialTheme.colorScheme.background,
+                    ),
+                    title = {
+                        Text(text = stringResource(R.string.app_name))
+                    }
+                )
+            },
+            content = { innerPadding ->
+                HiringScreen(modifier = Modifier.Companion.padding(innerPadding))
+            }
+        )
+    }
 }
